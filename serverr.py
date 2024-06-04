938d63b1d0d50357fcbe05224fb5e5a5508754a3
NEXUS-SPACE <120999573+YouTube-TW-MAIN@users.noreply.github.com>
2024-06-04 16:41:02 +0530

    Update serverr.py

--- serverr.py
+++ serverr.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('7202532685:AAGLATO-6cAbJUVHZL-6UFTtfzuLnWqgZPw')
+bot = telebot.TeleBot('7202601039:AAFBpcu9EXCMcZuek8VlTzvtiJbE3VWSrgw')
 
 # Admin user IDs
-admin_id = {"1283324622", "6406776405", "1600832237"}
+admin_id = {"6852753860", "1634950258", "6332240750"}
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
